36c32dfd7511b3ada7cbed500408d15803d400f2
Samuel-J <samuelj1519@gmail.com>
2025-07-18 14:17:30 +0800

    refactor: update HTTP server implementation to use the standard net/http package and simplify health check handler

--- http/health/server.go
+++ http/health/server.go
@@ -1,24 +1,24 @@
 package health
 
 import (
-	"net/http"
+	httpgo "net/http"
 
-	khttp "github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/go-kratos/swagger-api/openapiv2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Server struct {
-	*khttp.Server
+	*http.Server
 }
 
 func NewServer(addr string) *Server {
-	s := khttp.NewServer(khttp.Address(addr))
+	s := http.NewServer(http.Address(addr))
 
 	s.HandlePrefix("/q/", openapiv2.NewHandler())
 	s.Handle("/metrics", promhttp.Handler())
-	s.HandleFunc("/health", func(w khttp.ResponseWriter, r *khttp.Request) {
-		w.WriteHeader(http.StatusOK)
+	s.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(httpgo.StatusOK)
 	})
 
 	return &Server{s}
